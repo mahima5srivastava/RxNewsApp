e97dc8a7327a75165cbfd735de85cbce8651fb0d
Mahima Srivastava <mahima5srivastava@gmail.com>
2020-07-20 11:20:24 +0530

    News display

---
+++ RxNewsApp/Models/Article.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleModel.swift
+//  RxNewsApp
+//
+//  Created by MAHIMA on 20/07/20.
+//  Copyright © 2020 MAHIMA. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Codable {
+    let title: String?
+    let description: String?
+}
+
+struct ArticlesList: Codable {
+    let articles: [Article]?
+}
--- RxNewsApp/NewsViewController.swift
+++ RxNewsApp/NewsViewController.swift
@@ -7,37 +7,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsViewController: UITableViewController {
     
     //MARK:- IBOutelts
     
     @IBOutlet var newsTableView: UITableView!
-    
+    let disposeBag = DisposeBag()
+    var articles = [Article]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "News"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        populateNews()
     }
 
+    private func populateNews() {
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=78aed7e3e8c54c5db52e54370adffcd3") else {return}
+        Observable.just(url).flatMap { url -> Observable<Data> in
+            let request = URLRequest(url: url)
+            return URLSession.shared.rx.data(request: request)
+        }.map { data -> [Article]? in
+            return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
+            
+        }.subscribe(onNext: {[weak self] articles in
+            if let articles = articles {
+                self?.articles = articles
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }).disposed(by: disposeBag)
+            
+    }
+        
+        
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
+        return self.articles.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as? NewsCell else {return UITableViewCell()}
-
-      
-        cell.titleLabel.text = "News Title"
-        cell.descriptionLabel.text = "News Description"
+        cell.titleLabel.text = articles[indexPath.row].title
+        cell.descriptionLabel.text = articles[indexPath.row].description
         return cell
     }
 
